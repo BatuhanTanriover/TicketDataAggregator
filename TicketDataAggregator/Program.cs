36fbc4487258c4b3f5c896d274225c88c19bf2ca
Batuhan Tanrıöver <batu_2000_10@hotmail.com>
2025-02-23 23:32:31 +0300

    Add project files.

---
+++ TicketDataAggregator/Program.cs
@@ -0,0 +1,47 @@
+﻿using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+const string TicketsFolder = @"C:\Users\user\Desktop\Tickets";
+
+
+try
+{
+    var ticketsAggregator = new TicketAggregator(TicketsFolder);
+
+    ticketsAggregator.Run();
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An exception occured. " +
+       "Exception message: " + ex.Message);
+}
+
+public class TicketAggregator
+{
+    private readonly string _ticketsFolder;
+
+    public TicketAggregator(string ticketsFolder)
+    {
+        _ticketsFolder = ticketsFolder;
+    }
+
+    public void Run()
+    {
+        foreach (var filePath in Directory.GetFiles(_ticketsFolder, "*.pdf"))
+        {
+            using (PdfDocument document = PdfDocument.Open(filePath))
+            {
+
+
+                // Page number starts from 1, not 0.
+                Page page = document.GetPage(1);
+
+                string text = page.Text;
+            }
+        }
+
+
+    }
+
+}
